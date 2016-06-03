0c53ed6bcb31e54de0d187467023f5e1d5945655
Dracony <draconyster@gmail.com>
2016-06-03 11:47:13 +0200

    route handling

--- src/PHPixie/Route/Resolvers/Resolver/Group.php
+++ src/PHPixie/Route/Resolvers/Resolver/Group.php
@@ -7,43 +7,43 @@ class Group implements \PHPixie\Route\Resolvers\Resolver,
 {
     protected $resolverBuilder;
     protected $resolversConfig;
-    
+
     protected $names;
     protected $resolverMap;
-    
+
     public function __construct($resolverBuilder, $configData)
     {
         $this->resolverBuilder = $resolverBuilder;
         $this->resolversConfig = $configData->slice('resolvers');
     }
-    
+
     public function names()
     {
         $this->requireRouteNames();
         return $this->names;
     }
-    
+
     public function get($name)
     {
         $this->requireRouteNames();
-        
+
 
         if(!array_key_exists($name, $this->resolverMap)) {
-            throw new \PHPixie\Route\Exception\Route();
+            throw new \PHPixie\Route\Exception\Route("Route '$name' not found in this group.");
         }
-        
+
         if($this->resolverMap[$name] === null) {
             $configData = $this->resolversConfig->slice($name);
             $this->resolverMap[$name] = $this->resolverBuilder->buildFromConfig($configData);
         }
-        
+
         return $this->resolverMap[$name];
     }
-    
+
     public function match($segment)
     {
         $match = null;
-        
+
         foreach($this->names() as $name) {
             $resolver = $this->get($name);
             $match = $resolver->match($segment);
@@ -52,17 +52,17 @@ class Group implements \PHPixie\Route\Resolvers\Resolver,
                 break;
             }
         }
-        
+
         return $match;
     }
-    
+
     public function generate($match, $withHost = false)
     {
         $name  = $match->popResolverPath();
         $resolver = $this->get($name);
         return $resolver->generate($match, $withHost);
     }
-    
+
     protected function requireRouteNames()
     {
         if($this->names === null) {
